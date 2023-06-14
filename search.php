03da1b8a76cd7cfa55288897b8cd7498289f84b8
anasbekheit <anas.bekheit@gmail.com>
2023-06-14 11:04:10 +0300

    [Security] SQL Injection protection.

--- search.php
+++ search.php
@@ -18,51 +18,79 @@ if (isset($_GET['logout'])) {
  * @param $car_manufacturer
  * @param $car_country
  * @param $car_color
- * @return string
+ * @return array
  */
-function getWhere($car_model, $car_price, $car_manufacturer, $car_country, $car_color)
+function getWhere($car_model, $car_price, $car_manufacturer, $car_country, $car_color): array
 {
     $where = "";
+    $params = array();
+
     if (!empty($car_model)) {
-        $where .= "AND car_model = '$car_model' ";
+        $where .= "AND car_model = ? ";
+        $params[] = $car_model;
     }
     if (!empty($car_price)) {
-        $where .= "AND price_per_day <= '$car_price' ";
+        $where .= "AND price_per_day <= ? ";
+        $params[] = $car_price;
     }
     if (!empty($car_manufacturer)) {
-        $where .= "AND car_manufacturer ='$car_manufacturer' ";
+        $where .= "AND car_manufacturer = ? ";
+        $params[] = $car_manufacturer;
     }
     if (!empty($car_country)) {
-        $where .= "AND country='$car_country' ";
+        $where .= "AND country = ? ";
+        $params[] = $car_country;
     }
     if (!empty($car_color)) {
-        $where .= "AND color= '$car_color' ";
+        $where .= "AND color = ? ";
+        $params[] = $car_color;
     }
-    return $where;
+
+    // Prepare the WHERE clause
+    $where = ltrim($where, "AND ");
+    $where = $where ? "WHERE $where" : "";
+
+    return array(
+        'where' => $where,
+        'params' => $params
+    );
 }
 
 // receive all input values from the form
-$car_model = isset($_POST['car_model']) ? mysqli_real_escape_string($db, $_POST['car_model']) : "";
-$car_price = isset($_POST['car_price']) ? mysqli_real_escape_string($db, $_POST['car_price']) : "";
-$car_manufacturer = isset($_POST['car_manufacturer']) ? mysqli_real_escape_string($db, $_POST['car_manufacturer']) : "";
-$car_color = isset($_POST['car_color']) ? mysqli_real_escape_string($db, $_POST['car_color']) : "";
-$car_country = isset($_POST['car_country']) ? mysqli_real_escape_string($db, $_POST['car_country']) : "";
-$from_date = isset($_POST['from_date']) ? mysqli_real_escape_string($db, $_POST['from_date']) : "";
-$to_date = isset($_POST['to_date']) ? mysqli_real_escape_string($db, $_POST['to_date']) : "";
+$car_model = $_POST['car_model'] ?? "";
+$car_price = $_POST['car_price'] ?? "";
+$car_manufacturer = $_POST['car_manufacturer'] ?? "";
+$car_color = $_POST['car_color'] ?? "";
+$car_country = $_POST['car_country'] ?? "";
+$from_date = $_POST['from_date'] ?? "";
+$to_date = $_POST['to_date'] ?? "";
 
 $query = "SELECT plate_id,car_model,car_manufacturer,color,model_year,country,price_per_day,car_status,car_image FROM car  WHERE car_status = 'active' ";
-$where = getWhere($car_model, $car_price, $car_manufacturer, $car_country, $car_color);
-$query .= $where;
+$whereData = getWhere($car_model, $car_price, $car_manufacturer, $car_country, $car_color);
+$query .= $whereData['where'];
 $query .= "EXCEPT (SELECT plate_id,car_model,car_manufacturer,color,model_year,country,price_per_day,car_status,car_image
-FROM reservation NATURAL JOIN car WHERE '$from_date' BETWEEN pickup_time AND return_time OR '$to_date' BETWEEN pickup_time AND return_time)";
-$results = mysqli_query($db, $query);
+FROM reservation NATURAL JOIN car WHERE ? BETWEEN pickup_time AND return_time OR ? BETWEEN pickup_time AND return_time)";
 
-if ($results) {
-    $rows = $results->fetch_all(MYSQLI_ASSOC);
-    $_SESSION['from_date'] = $from_date;
-    $_SESSION['to_date'] = $to_date;
+$statement = mysqli_prepare($db, $query);
 
+if ($statement) {
+    // Bind the parameters
+    $bindParams = array_merge(array(str_repeat("s", count($whereData['params']))), $whereData['params'], array($from_date, $to_date));
+    mysqli_stmt_bind_param($statement, ...$bindParams);
+
+    // Execute the statement
+    mysqli_stmt_execute($statement);
+
+    // Get the results
+    $results = mysqli_stmt_get_result($statement);
+
+    if ($results) {
+        $rows = $results->fetch_all(MYSQLI_ASSOC);
+        $_SESSION['from_date'] = $from_date;
+        $_SESSION['to_date'] = $to_date;
+
+    }
 }
 // Return the data as JSON
 header('Content-Type: application/json');
-echo json_encode(isset($rows)? $rows : []);
+echo json_encode($rows ?? []);
--- server.php
+++ server.php
@@ -2,8 +2,81 @@
 if (session_id() === '') {
     session_start(); // Only start the session if it is not already active
 }
-$errors=array();
+$errors = array();
 require 'errors.php';
+
 // connect to the database
 $db = mysqli_connect('localhost', 'root', '', 'car_rental_system');
 
+// Function to check if a user with the given email already exists
+function alreadyExists($email): bool
+{
+    global $db;
+
+    $query = "SELECT * FROM customer WHERE email=?";
+    $statement = mysqli_prepare($db, $query);
+    mysqli_stmt_bind_param($statement, "s", $email);
+    mysqli_stmt_execute($statement);
+    mysqli_stmt_store_result($statement);
+
+    return mysqli_stmt_num_rows($statement) > 0;
+}
+
+// Function to insert a new user into the database
+function registerUser($fname, $lname, $email, $password, $country, $credit_card): array
+{
+    global $db;
+
+    $hashedPassword = md5($password);
+
+    $query = "INSERT INTO customer (fname, lname, email, customer_pass, country, credit_card) 
+              VALUES (?, ?, ?, ?, ?, ?)";
+    $statement = mysqli_prepare($db, $query);
+    mysqli_stmt_bind_param($statement, "ssssss", $fname, $lname, $email, $hashedPassword, $country, $credit_card);
+    mysqli_stmt_execute($statement);
+
+    $user = [
+        'email' => $email,
+        'fname' => $fname,
+        'lname' => $lname,
+        'country' => $country,
+        'credit_card' => $credit_card
+    ];
+
+    return $user;
+}
+
+// Function to validate the user's credentials
+function validateCredentials($email, $password)
+{
+    global $db;
+
+    if (empty($email) || empty($password)) {
+        return null;
+    }
+
+    $hashedPassword = md5($password);
+
+    $query = "SELECT customer_id, email, fname, lname, country, credit_card FROM customer WHERE email=? AND customer_pass=?";
+    $statement = mysqli_prepare($db, $query);
+    mysqli_stmt_bind_param($statement, "ss", $email, $hashedPassword);
+    mysqli_stmt_execute($statement);
+    mysqli_stmt_store_result($statement);
+
+    if (mysqli_stmt_num_rows($statement) == 1) {
+        mysqli_stmt_bind_result($statement, $customer_id, $email, $fname, $lname, $country, $credit_card);
+        mysqli_stmt_fetch($statement);
+
+        $user = [
+            'email' => $email,
+            'fname' => $fname,
+            'lname' => $lname,
+            'country' => $country,
+            'credit_card' => $credit_card
+        ];
+
+        return $user;
+    }
+
+    return null;
+}
