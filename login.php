2873848704afa8301c01689ca1ef0f0252701faa
anasbekheit <anas.bekheit@gmail.com>
2023-06-14 05:26:16 +0300

    [performance] removed the need to query twice inorder to login.

--- login.php
+++ login.php
@@ -1,24 +1,27 @@
 <?php
+/**
+ * @var mysqli $db
+ */
 include('server.php'); // Include the server-side code
 
 // Check if the form was submitted
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    global $db;
     $data = file_get_contents("php://input");
     $form_data = json_decode($data, true);
     $email = mysqli_real_escape_string($db, $form_data['login_email']);
     $password = mysqli_real_escape_string($db, $form_data['login_password']);
 
     // Validate the user's credentials
-    if (validateCredentials($email, $password)) {
-        $_SESSION['user'] = getUser($email);
+    $user = validateCredentials($email, $password);
+    if ($user !== null) {
+        $_SESSION['user'] = $user;
         $_SESSION['success'] = "You are now logged in";
         header("HTTP/1.1 200 OK");
     } else {
         header('Content-Type: application/json');
         header("HTTP/1.1 401 Unauthorized");
     }
-    exit();
+    exit;
 }
 
 // Function to validate the user's credentials
@@ -32,13 +35,5 @@ function validateCredentials($email, $password) {
     $results = mysqli_query($db, $query);
     $user = mysqli_fetch_assoc($results);
 
-    return mysqli_num_rows($results) == 1;
-}
-
-// Function to fetch user details based on email
-function getUser($email) {
-    global $db;
-    $query = "SELECT customer_id, email, fname, lname, country, credit_card FROM customer WHERE email='$email'";
-    $result = mysqli_query($db, $query);
-    return mysqli_fetch_assoc($result);
+    return mysqli_num_rows($results) == 1 ? $user: null;
 }
