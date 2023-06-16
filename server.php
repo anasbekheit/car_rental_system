6fcf212705e9a603dfe3a1f863ae0eb2e15e0865
anasbekheit <anas.bekheit@gmail.com>
2023-06-16 09:22:02 +0300

    [Deleted] errors.php

--- server.php
+++ server.php
@@ -3,7 +3,6 @@ if (session_id() === '') {
     session_start(); // Only start the session if it is not already active
 }
 $errors = array();
-require 'errors.php';
 
 // connect to the database
 $db = mysqli_connect('localhost', 'root', '', 'car_rental_system');
