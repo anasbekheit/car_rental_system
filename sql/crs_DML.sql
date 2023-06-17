93b9188e0b5a11befdfb8fe0053ffcc02db7023c
anasbekheit <anas.bekheit@gmail.com>
2023-06-17 04:40:01 +0300

    Removed extra space.

--- sql/crs_DML.sql
+++ sql/crs_DML.sql
@@ -1,10 +1,7 @@
-
-
 INSERT INTO `car` (`plate_id`, `car_status`, `color`, `model_year`, `car_model`, `car_manufacturer`, `country`, `price_per_day`) VALUES
 ('1111-AAAA', 'active', 'RED', 2021, 'Enzo', 'Ferrari', 'Italy', 1200.00),
 ('2222-AAAA', 'active', 'BLACK', 2015, '520i', 'BMW', 'Germany', 400.00),
 ('3333-AAAA', 'active', 'WHITE', 1980, 'BRUH', 'LADA', 'RUSSIA', 50.00);
-
 
 INSERT INTO `customer` ( `email`, `customer_pass`, `fname`, `lname`, `country`, `credit_card`) VALUES
 ( 'anas@gmail.com', '202cb962ac59075b964b07152d234b70', 'Anas', 'Ahmed', 'EG', '123'),
@@ -12,6 +9,5 @@ INSERT INTO `customer` ( `email`, `customer_pass`, `fname`, `lname`, `country`, 
 ('hazem@gmail.com', '202cb962ac59075b964b07152d234b70', 'Hazem', 'Hashem', 'EG', '123'),
 ('menna@gmail.com', '202cb962ac59075b964b07152d234b70', 'Menna', 'Ibrahim', 'EG', '123');
 
-
-    INSERT INTO `reservation` (`plate_id`, `customer_id`, `pickup_time`, `return_time`) VALUES
-    ('3333-AAAA', 1, '2022-01-05', '2022-01-09');
+INSERT INTO `reservation` (`plate_id`, `customer_id`, `pickup_time`, `return_time`) VALUES
+('3333-AAAA', 1, '2022-01-05', '2022-01-09');
